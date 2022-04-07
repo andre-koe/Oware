da1d021285550872e1cc5ce0928cbc708ce6c803
andre-koe <andre.koeniger1997@gmail.com>
2022-04-07 18:07:24 +0200

    forgot to update those files

--- src/main/scala/de/htwg/se/oware/Oware.scala
+++ src/main/scala/de/htwg/se/oware/Oware.scala
@@ -14,17 +14,7 @@ TODO: Aufruf an diese Sturktur annähern
 val eol = System.lineSeparator;
 
 @main def init: Unit =
-<<<<<<< HEAD
-        //test
-        //println(eol + "Welcome to Oware!" + eol)
-        //println(mesh(3, cellNum = len_board))
-        val f = new Field(6)
-        f.seed_from(7)
-        f.cell(2)
-        printf(f.toString)
-=======
         println(eol + "Welcome to Oware!" + eol)
         val f = new Field(1)
         println(f)
->>>>>>> developer
 
