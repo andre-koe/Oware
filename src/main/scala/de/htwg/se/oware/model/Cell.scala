f1a1351ab8d311a08c1b43d3fda161f910b29072
andre-koe <andre.koeniger1997@gmail.com>
2022-04-12 20:02:38 +0200

    Updated isSnackable in Cell.scala

--- src/main/scala/de/htwg/se/oware/model/Cell.scala
+++ src/main/scala/de/htwg/se/oware/model/Cell.scala
@@ -3,7 +3,7 @@ package de.htwg.se.oware.model
 
 case class Cell(value: Int) extends Ordered[Cell]{
   def isEmpty: Boolean = value == 0
-  def isSnackable: Boolean = value <= 2 && value > 0
+  def isSnackable: Boolean = value <= 2 && value >= 1
   def add: Cell = Cell(value + 1)
   def empty: Cell = Cell(0)
   override def compare(o: Cell) = 
