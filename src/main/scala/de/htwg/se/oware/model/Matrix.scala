f6b87abf5352fdf0d618f9bd5e67a182a9c4ce0d
andre-koe <andre.koeniger1997@gmail.com>
2022-04-22 11:17:16 +0200

    Improved populate_new

--- src/main/scala/de/htwg/se/oware/model/Matrix.scala
+++ src/main/scala/de/htwg/se/oware/model/Matrix.scala
@@ -5,37 +5,25 @@ case class Matrix(cols: Vector[Cell]):
     def total_length: Int = cols.size
     def row_length: Int = if ((total_length % 2) == 0) (total_length/2)  else 0
     def cell(index: Int): Cell  = cols(index % total_length)
-    /* FIX THIS SHIT! */
+    
+    /* Alternative but complex solution without the use of mutable datatypes */
+    /* Currently not working */
     def populate_new(index: Int, value: Int): Matrix = {
         var temp_array: Array[Cell] = new Array(total_length)
         cols.copyToArray(temp_array)
-        var just_started = true
         for {
             i <- index until index + temp_array(index).value + 1
         } 
-        if (i % total_length == index && just_started) then
-            temp_array(i) = Cell(0)
-            just_started = false
-        else 
-            temp_array(i % total_length) = temp_array(i % total_length).add
+        temp_array(i % total_length) = temp_array(i % total_length).add
 
-        Matrix(temp_array.toVector)
-    }
-
-    /*
-    /* Alternative but complex solution without the use of mutable datatypes */
-    /* Currently not working */
-    def populate_new_FP(index: Int, value: Int): Matrix = {
         val zippedWithIndex = cols.zipWithIndex
         val idx = index % total_length
         val new_vector: Vector[Cell] = zippedWithIndex.map(
             i => 
-            if (i._2 == idx) then Cell(cell(i._2).value / total_length)
-            else if ((i._2 > index && i._2 <= index + value) || 
-                (i._2 <= (cell(index).value - (row_length - (index % total_length) % row_length)))) then Cell(cell(i._2).value).add
-            else cell(i._2)
+                if (i._2 == idx) then Cell(cell(i._2).value / total_length)
+                else Cell(temp_array(i._2).value)
         )
         Matrix(new_vector)
     }
-    */
+    
 
